02036a84aa82d28a00012b6bed557f6a4d9d00a0
Liam <79233903+WilliammGalvin@users.noreply.github.com>
2024-10-22 23:22:31 -0400

    Update ProjectCard.tsx

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -34,7 +34,7 @@ export default function ProjectCard({
         <div className="mt-12">
           <iframe
             className="rounded-lg w-full min-h-[300px]"
-            src="https://www.youtube.com/embed/nI_7Oul1OTc?si=ruNW1NV9tcLFqlbc"
+            src={youtubeUrl}
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerPolicy="strict-origin-when-cross-origin"
